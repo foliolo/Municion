9eea1cfbac61e59485f20f8d63d412ab732b737a
david.sierra <dasies11@gmail.com>
2016-07-26 08:12:27 +0200

    Agregado en pruebas las notificaciones para las licencias. Esta en una fase inicial. Es necesario añadir la logica de comprobación de las fechas proximas a caducar

---
+++ app/src/main/java/al/ahgitdevelopment/municion/ReceiverBroad.java
@@ -0,0 +1,18 @@
+package al.ahgitdevelopment.municion;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by david.sierra on 25/07/2016.
+ */
+public class ReceiverBroad extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent service = new Intent(context, AlarmService.class);
+        context.startService(service);
+
+    }
+}
