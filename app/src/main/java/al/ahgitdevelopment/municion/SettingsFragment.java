af65b456e79b5f34b72e6b22591f926b9ac43396
Alberto <foliolo7891@gmail.com>
2017-01-27 01:39:20 +0100

    * MODIF: Control de errores en los setting mediante el uso de try/catch

--- app/src/main/java/al/ahgitdevelopment/municion/SettingsFragment.java
+++ app/src/main/java/al/ahgitdevelopment/municion/SettingsFragment.java
@@ -16,6 +16,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.crash.FirebaseCrash;
+
 import java.security.SecureRandom;
 import java.util.ArrayList;
 
@@ -141,99 +143,119 @@ public class SettingsFragment extends FragmentActivity implements IabHelper.Quer
 
     @Override
     public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
-        // if we were disposed of in the meantime, quit.
-        if (mHelper == null) return;
+        try {
+            // if we were disposed of in the meantime, quit.
+            if (mHelper == null) return;
 
-        if (result.isFailure()) {
-            Log.e(TAG, "Error obteniendo los detalles de productos" + result.getMessage());
-            return;
-        }
-
-        if (inventory.hasPurchase(PURCHASE_ID_REMOVE_ADS)) {
-            //pero no tiene actualizado su shared prefs
-            if (prefs.getBoolean("show_ads", true)) {
-                // Actualizamos las preferencias
-                prefs.edit().putBoolean(PREFS_SHOW_ADS, false).apply();
+            if (result.isFailure()) {
+                Log.e(TAG, "Error obteniendo los detalles de productos" + result.getMessage());
+                return;
             }
-            Toast.makeText(SettingsFragment.this, R.string.purchase_done, Toast.LENGTH_SHORT).show();
 
-            //TODO: Remove this!!! Just for testing
-            if (flagConsume) {
-                mHelper.consumeAsync(inventory.getPurchase(PURCHASE_ID_REMOVE_ADS), this);
-                prefs.edit().putBoolean(PREFS_SHOW_ADS, true).apply();
+            if (inventory.hasPurchase(PURCHASE_ID_REMOVE_ADS)) {
+                //pero no tiene actualizado su shared prefs
+                if (prefs.getBoolean("show_ads", true)) {
+                    // Actualizamos las preferencias
+                    prefs.edit().putBoolean(PREFS_SHOW_ADS, false).apply();
+                }
+                Toast.makeText(SettingsFragment.this, R.string.purchase_done, Toast.LENGTH_SHORT).show();
+
+                //TODO: Remove this!!! Just for testing
+                if (flagConsume) {
+                    mHelper.consumeAsync(inventory.getPurchase(PURCHASE_ID_REMOVE_ADS), this);
+                    prefs.edit().putBoolean(PREFS_SHOW_ADS, true).apply();
+                }
+            } else {
+                //Generar PREFS_PAYLOAD del usuario: Numero aleatorio que identifica al usuario
+                SecureRandom random = new SecureRandom();
+                String payload = new java.math.BigInteger(130, random).toString(32);
+                if (!prefs.contains(PREFS_PAYLOAD))
+                    prefs.edit().putString(PREFS_PAYLOAD, payload).apply();
+                else
+                    payload = prefs.getString(PREFS_PAYLOAD, "");
+
+                // Realizar compra para eliminar publicidad
+                mHelper.launchPurchaseFlow(this, PURCHASE_ID_REMOVE_ADS, RC_PURCHASE_FLOW,
+                        this /*PurchaseFinishedListener*/, payload);
             }
-        } else {
-            //Generar PREFS_PAYLOAD del usuario: Numero aleatorio que identifica al usuario
-            SecureRandom random = new SecureRandom();
-            String payload = new java.math.BigInteger(130, random).toString(32);
-            if (!prefs.contains(PREFS_PAYLOAD))
-                prefs.edit().putString(PREFS_PAYLOAD, payload).apply();
-            else
-                payload = prefs.getString(PREFS_PAYLOAD, "");
-
-            // Realizar compra para eliminar publicidad
-            mHelper.launchPurchaseFlow(this, PURCHASE_ID_REMOVE_ADS, RC_PURCHASE_FLOW,
-                    this /*PurchaseFinishedListener*/, payload);
+        } catch (Exception ex) {
+            FirebaseCrash.logcat(Log.ERROR, TAG, ex.getMessage());
+            FirebaseCrash.report(ex);
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + "," + data);
-        if (mHelper == null) return;
+        try {
+            Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + "," + data);
+            if (mHelper == null) return;
 
-        // Pass on the activity result to the helper for handling
-        if (!mHelper.handleActivityResult(requestCode, resultCode, data)) {
-            // not handled, so handle it ourselves (here's where you'd
-            // perform any handling of activity results not related to in-app
-            // billing...
-            super.onActivityResult(requestCode, resultCode, data);
-        } else {
-            Log.d(TAG, "onActivityResult handled by IABUtil.");
+            // Pass on the activity result to the helper for handling
+            if (!mHelper.handleActivityResult(requestCode, resultCode, data)) {
+                // not handled, so handle it ourselves (here's where you'd
+                // perform any handling of activity results not related to in-app
+                // billing...
+                super.onActivityResult(requestCode, resultCode, data);
+            } else {
+                Log.d(TAG, "onActivityResult handled by IABUtil.");
+            }
+        } catch (Exception ex) {
+            FirebaseCrash.logcat(Log.ERROR, TAG, ex.getMessage());
+            FirebaseCrash.report(ex);
         }
     }
 
     @Override
     public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
-        Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+        try {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
 
-        // if we were disposed of in the meantime, quit.
-        if (mHelper == null) return;
+            // if we were disposed of in the meantime, quit.
+            if (mHelper == null) return;
 
-        if (result.isFailure()) {
-            Log.d(TAG, "Error purchasing: " + result.toString());
-            new AlertDialog.Builder(SettingsFragment.this)
-                    .setTitle(getString(R.string.purchase_cancel))
-                    .setPositiveButton(android.R.string.ok, null)
-                    .setIcon(R.drawable.ic_warning_amber_48pt_3x)
-                    .show();
-            return;
-        }
+            if (result.isFailure()) {
+                Log.d(TAG, "Error purchasing: " + result.toString());
+                new AlertDialog.Builder(SettingsFragment.this)
+                        .setTitle(getString(R.string.purchase_cancel))
+                        .setPositiveButton(android.R.string.ok, null)
+                        .setIcon(R.drawable.ic_warning_amber_48pt_3x)
+                        .show();
+                return;
+            }
 
-        // TASK TO DO
-        String payload = "";
-        if (prefs.contains(PREFS_PAYLOAD))
-            payload = prefs.getString(PREFS_PAYLOAD, "");
+            // TASK TO DO
+            String payload = "";
+            if (prefs.contains(PREFS_PAYLOAD))
+                payload = prefs.getString(PREFS_PAYLOAD, "");
 
-        if (purchase.getSku().equals(PURCHASE_ID_REMOVE_ADS) && purchase.getDeveloperPayload().equals(payload)) {
-            // Compra realizada con existo
-            // Actualizar Shared Prefs
-            prefs.edit().putBoolean(PREFS_SHOW_ADS, false).apply();
+            if (purchase.getSku().equals(PURCHASE_ID_REMOVE_ADS) && purchase.getDeveloperPayload().equals(payload)) {
+                // Compra realizada con existo
+                // Actualizar Shared Prefs
+                prefs.edit().putBoolean(PREFS_SHOW_ADS, false).apply();
+            }
+        } catch (Exception ex) {
+            FirebaseCrash.logcat(Log.ERROR, TAG, ex.getMessage());
+            FirebaseCrash.report(ex);
         }
     }
 
     @Override
     public void onConsumeFinished(Purchase purchase, IabResult result) {
-        if (result.isSuccess()) {
-            // provision the in-app purchase to the user
-            // (for example, credit 50 gold coins to player's character)
-            Log.i(TAG, "Compra consumida");
-            Toast.makeText(this, "Compra consumida", Toast.LENGTH_SHORT).show();
-            prefs.edit().putBoolean(PREFS_SHOW_ADS, true).apply();
-        } else {
-            // handle error
-            Log.w(TAG, "Error cosumiendo la compra!");
-            Toast.makeText(this, "Error consumiendo compra", Toast.LENGTH_SHORT).show();
+        try {
+            if (result.isSuccess()) {
+                // provision the in-app purchase to the user
+                // (for example, credit 50 gold coins to player's character)
+                Log.i(TAG, "Compra consumida");
+                Toast.makeText(this, "Compra consumida", Toast.LENGTH_SHORT).show();
+                prefs.edit().putBoolean(PREFS_SHOW_ADS, true).apply();
+            } else {
+                // handle error
+                Log.w(TAG, "Error cosumiendo la compra!");
+                Toast.makeText(this, "Error consumiendo compra", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception ex) {
+            FirebaseCrash.logcat(Log.ERROR, TAG, ex.getMessage());
+            FirebaseCrash.report(ex);
         }
     }
 }
