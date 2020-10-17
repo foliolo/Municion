60b6272bc7c92eb231ba30c767e9f63c2a48e52c
foliolo <foliolo7891@gmail.com>
2020-10-17 01:07:18 +0100

    Hide drawer navigation bar on tutorial screens

--- app/src/main/java/al/ahgitdevelopment/municion/ui/tutorial/TutorialViewPagerFragment.kt
+++ app/src/main/java/al/ahgitdevelopment/municion/ui/tutorial/TutorialViewPagerFragment.kt
@@ -33,7 +33,11 @@ class TutorialViewPagerFragment : Fragment() {
         AppComponent.create(requireContext()).inject(this)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         val uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
@@ -67,6 +71,12 @@ class TutorialViewPagerFragment : Fragment() {
 
         val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view?.windowToken, 0)
+
+        hiddenDrawerNavigationMenu()
+    }
+
+    private fun hiddenDrawerNavigationMenu() {
+        requireActivity().nav_view.visibility = View.GONE
     }
 
     class SectionsPagerAdapter(
