1ad54cb654537183bcb74a8918031594493cf151
foliolo <foliolo7891@gmail.com>
2020-12-07 00:18:20 +0000

    Integrated Admob feature

--- app/src/main/java/al/ahgitdevelopment/municion/NavigationActivity.kt
+++ app/src/main/java/al/ahgitdevelopment/municion/NavigationActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdRequest
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_navigation.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -62,6 +63,11 @@ class NavigationActivity : AppCompatActivity() {
                     nav_view.visibility = View.VISIBLE
                 }
             }
+        }
+
+        AdRequest.Builder().build().let {
+            adView.adListener = ManageAds(adView)
+            adView.loadAd(it)
         }
     }
 
