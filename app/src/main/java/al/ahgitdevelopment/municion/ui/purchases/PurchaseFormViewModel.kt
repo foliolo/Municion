b84875fe8b5ce6bf870279bf18c428f9549b2cad
foliolo <foliolo7891@gmail.com>
2024-01-07 11:08:52 +0100

    Update dependencies and fix issues in mutable

--- app/src/main/java/al/ahgitdevelopment/municion/ui/purchases/PurchaseFormViewModel.kt
+++ app/src/main/java/al/ahgitdevelopment/municion/ui/purchases/PurchaseFormViewModel.kt
@@ -4,6 +4,7 @@ import al.ahgitdevelopment.municion.datamodel.Purchase
 import al.ahgitdevelopment.municion.repository.RepositoryContract
 import al.ahgitdevelopment.municion.utils.SingleLiveEvent
 import android.view.View
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,23 +16,28 @@ class PurchaseFormViewModel @Inject constructor(
     private val repository: RepositoryContract,
 ) : ViewModel() {
 
-    val fabSavePurchaseClicked = SingleLiveEvent<Unit>()
-    val date = SingleLiveEvent<Unit>()
-    val closeForm = SingleLiveEvent<Unit>()
+    private val _fabSavePurchaseClicked = SingleLiveEvent<Unit>()
+    val fabSavePurchaseClicked:LiveData<Unit> = _fabSavePurchaseClicked
+
+    private val _date = SingleLiveEvent<Unit>()
+    val date: LiveData<Unit> = _date
+
+    private val _closeForm = SingleLiveEvent<Unit>()
+    val closeForm :LiveData<Unit> = _closeForm
 
     fun fabSavePurchaseClicked(view: View) {
-        fabSavePurchaseClicked.call()
+        _fabSavePurchaseClicked.call()
     }
 
     fun savePurchase(purchase: Purchase) {
         // TODO: perform field validations
         viewModelScope.launch {
             repository.savePurchase(purchase)
-            closeForm.call()
+            _closeForm.call()
         }
     }
 
     fun selectDate(view: View) {
-        date.call()
+        _date.call()
     }
 }
